e593ceb8c35c52ba682e6dfb63c47a6c82459a58
Sergi <Sergi@host.docker.internal>
2024-05-10 17:59:32 +0200

    exercise 2 finished

--- src/main/java/principal/controllers/DatosDeTabla.java
+++ src/main/java/principal/controllers/DatosDeTabla.java
@@ -16,54 +16,47 @@ public class DatosDeTabla {
 	}
 
 	public static Object[][] getDatosDeTablaFiltrado(String str) {
-	    if (datos == null) {
-	        if (PanelTablaYFicheros.getCarpeta() != null) {
-	            File[] estudiantes = PanelTablaYFicheros.getCarpeta().listFiles();
-	            datos = new Object[estudiantes.length][3];
-	            int contador = 0; // Variable para controlar la posición en la matriz de datos
-	            for (int i = 0; i < estudiantes.length; i++) {
-	                String nombre = estudiantes[i].getName();
-	                if (nombre.toLowerCase().contains(str)) {
-	                    // Si el nombre coincide con el filtro, añadir datos a la matriz
-	                    datos[contador][0] = nombre;
-	                    datos[contador][1] = (estudiantes[i].length() / 1024f + " KB");
-	                    Date d = new Date(estudiantes[i].lastModified());
-	                    SimpleDateFormat dft = new SimpleDateFormat("dd/MM/yyyy");
-	                    datos[contador][2] = dft.format(d);
-	                    contador++; // Incrementar el contador de posición
-	                }
+	    if (PanelTablaYFicheros.getCarpeta() != null) {
+	        File[] estudiantes = PanelTablaYFicheros.getCarpeta().listFiles();
+	        datos = new Object[estudiantes.length][3];
+	        int contador = 0;
+	        for (int i = 0; i < estudiantes.length; i++) {
+	            String nombre = estudiantes[i].getName();
+	            if (nombre.contains(str)) {
+	                datos[contador][0] = nombre;
+	                datos[contador][1] = (estudiantes[i].length() / 1024f + " KB");
+	                Date d = new Date(estudiantes[i].lastModified());
+	                SimpleDateFormat dft = new SimpleDateFormat("dd/MM/yyyy");
+	                datos[contador][2] = dft.format(d);
+	                contador++;
 	            }
-	            // Redimensionar la matriz para eliminar las filas vacías
-	            Object[][] datosFiltrados = new Object[contador][3];
-	            for (int i = 0; i < contador; i++) {
-	                datosFiltrados[i] = datos[i];
-	            }
-	            return datosFiltrados;
+	        }
+	        // Redimensionar la matriz para eliminar las filas vacías
+	        Object[][] datosFiltrados = new Object[contador][3];
+	        for (int i = 0; i < contador; i++) {
+	            datosFiltrados[i] = datos[i];
+	        }
+	        return datosFiltrados;
+	    }
+	    return datos;
+	}
+
+
+	public static Object[][] getDatosDeTabla() {
+	    if (PanelTablaYFicheros.getCarpeta() != null) {
+	        File[] estudiantes = PanelTablaYFicheros.getCarpeta().listFiles();
+	        datos = new Object[estudiantes.length][3];
+	        for (int i = 0; i < estudiantes.length; i++) {
+	            File e = estudiantes[i];
+	            datos[i][0] = e.getName();
+	            datos[i][1] = (e.length() / 1024f + " KB");
+	            Date d = new Date(e.lastModified());
+	            SimpleDateFormat dft = new SimpleDateFormat("dd/MM/yyyy");
+	            datos[i][2] = dft.format(d);
 	        }
 	    }
 	    return datos;
 	}
 
-	public static Object[][] getDatosDeTabla() {
-		if (datos == null) {
-			if (PanelTablaYFicheros.getCarpeta() != null) {
-				File[] estudiantes = PanelTablaYFicheros.getCarpeta().listFiles();
-				datos = new Object[estudiantes.length][3];
-				for (int i = 0; i < estudiantes.length; i++) {
-					File e = estudiantes[i];
-					datos[i][0] = e.getName();
-					datos[i][1] = (e.length() / 1024f + " KB");
-					Date d = new Date(e.lastModified());
-					SimpleDateFormat dft = new SimpleDateFormat("dd/MM/yyyy");
-					;
-					datos[i][2] = dft.format(d);
-
-				}
-			}
-
-		}
-
-		return datos;
-	}
 
 }
